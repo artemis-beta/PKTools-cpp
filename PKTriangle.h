232cf7f2021a7eaac6e1ca3568ee02ac26beef80
artemis-beta <KXZ186@student.bham.ac.uk>
2016-07-28 23:23:32 +0100

    PKTriangle Class for Sandbox

---
+++ PKTriangle.h
@@ -0,0 +1,24 @@
+#ifndef __PKTRIANGLE__
+#define __PKTRIANGLE__
+
+#include <string>
+#include <cmath>
+
+const double pi = 4*atan(1);
+
+class PKTriangle {
+
+	private:
+		std::string type_;
+		double sides[3];
+		double angles[3];
+		void calculate_(double par1,double par2, double par3);
+	public:
+		PKTriangle(double par1,double par2,double par3,std::string opts);	
+		double* GetSides();
+		double* GetAngles();
+		void Print();		
+};
+
+#endif
+
