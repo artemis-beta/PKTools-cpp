4f5fd513c3b707925a0ab2c02c753246aedc0845
artemis-beta <KXZ186@student.bham.ac.uk>
2016-08-01 20:54:18 +0100

    Major Corrections for Calculation

--- PKTriangle.cpp
+++ PKTriangle.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
 #include <cmath>
 #include "PKTriangle.h"
+
+double CosineRuleSide(double side1, double side2, double angle)
+{return pow(pow(side1,2)+pow(side2,2) - 2*side1*side2*cos(angle),0.5);}
+double CosineRuleAngle(double side1, double side2, double side3)
+{return acos((pow(side1,2)+pow(side2,2)-pow(side3,2))/(2*side1*side2));}
 void PKTriangle::calculate_(double par1,double par2,double par3)
 {
 	if(type_ == "AAS")
 	{
-		angles[0] = par1;
-		angles[1] = par2;
-		sides[2] = par3;
+		angles[0] = par1*1.0;
+		angles[1] = par2*1.0;
+		sides[2] = par3*1.0;
 		
 		angles[2] = pi - (par1+par2);
 		sides[1] = (sides[2]/sin(angles[2]))*sin(angles[1]);
-		sides[0] = (sides[2]/sin(angles[2]))*sin(sides[0]);
+		sides[0] = (sides[2]/sin(angles[2]))*sin(angles[0]);
 	}
-	else{
-		sides[0] = par1;
-		sides[1] = par2;
-		angles[2] = par3;
+	else
+	{
+		sides[0] = par1*1.0;
+		sides[1] = par2*1.0;
+		angles[2] = par3*1.0;
 
-		sides[2] = pow(sides[0],2)+pow(sides[1],2) - 2*sides[0]*sides[1]*cos(angles[2]);
-		angles[0] = asin((angles[2]/sides[2])*sides[0]);
-		angles[1] = asin((angles[2]/sides[2])*sides[1]);
+		sides[2] = CosineRuleSide(sides[0],sides[1],angles[2]);
+		angles[0] = asin((sin(angles[2])/sides[2])*sides[0]);
+		angles[1] = asin((sin(angles[2])/sides[2])*sides[1]);
 	}
 }		
 		 
@@ -39,5 +45,8 @@ void PKTriangle::Print()
 	std::cout << "PKTriangle_" << type_ << "(ANGLES[" << angles[0] << "," << angles[1] << "," << angles[2] << "]";
 	std::cout << ", SIDES[" << sides[0] << "," << sides[1] << "," << sides[2] << "])\n";
 }
-double* PKTriangle::GetAngles(){return angles;}
-double* PKTriangle::GetSides(){return sides;}
+bool PKTriangle::isRightAngled()
+{
+	for(int i = 0; i<sizeof(angles);++i){if(angles[i] > 0.5*0.99*pi && angles[i] < 0.5*1.01*pi){return true;}}
+	return false;
+}
